4ad4afd9d30dd157dbdf2501c55b2eaefe845735
SantoshNarasimha <104484803+SantoshNarasimha@users.noreply.github.com>
2026-07-07 12:27:53 +0530

    Added new java files

--- duplicates.java
+++ duplicates.java
@@ -1,3 +1,5 @@
+// return the duplicate elements from list
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -5,11 +7,22 @@ class duplicates {
 
     public static void main(String[] args) {
 
-        List<Integer> nums = List.of(1, 2, 3, 2, 1, 4, 5, 6, 7, 8);
+        List<Integer> nums = new ArrayList<>(List.of(1, 2, 3, 2, 1, 4, 5, 6, 7, 8));
 
         Map<Integer, Long> a = nums.stream().collect(Collectors.groupingBy(n -> n, Collectors.counting()));
 
-        a.forEach((key, value) -> System.out.println(key + " " + value));
+        // Assuming your map is called 'myMap'
+        Set<Integer> b = a.entrySet().stream().filter(entry -> entry.getValue()  1).map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+
+        System.out.println(b);
+
+        // another way to print duplicate elements
+        // a.keySet().forEach(System.out::println);
+
+        // regular way to print
+        // a.forEach((key, value) -> System.out.println(key));
+        
 
     }
 
