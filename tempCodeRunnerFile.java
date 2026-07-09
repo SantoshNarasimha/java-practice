13aa4a71f0c90d89860901f8dee493dcc1bd6fb0
SantoshNarasimha <104484803+SantoshNarasimha@users.noreply.github.com>
2026-07-09 11:08:46 +0530

    Added new Java files

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,29 @@
-List<String> word = List.of(words);
+import java.util.List;
+
+class tempCodeRunnerFile {
+
+    static class Employee {
+
+        private String name;
+
+        public Employee(String name) {
+
+            this.name = name;
+
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = List.of(new Employee("Santosh"), new Employee("Narasimha"),
+                new Employee("Yogi"));
+
+        System.out.println(employees);
+
+    }
+
+}
